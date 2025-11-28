fcbde8a6c7ae3e19530d908318198e23aaa6fdd8
Dan <dan@eighttrigrams.net>
2025-11-28 22:08:14 +0000

    Add version with tight audio

--- src/daw/core.clj
+++ src/daw/core.clj
@@ -1,27 +1,73 @@
 (ns daw.core
-  (:import [javax.sound.sampled AudioSystem Clip]))
+  (:import [javax.sound.sampled AudioSystem AudioFormat SourceDataLine DataLine$Info AudioFormat$Encoding]))
 
-(defn load-clip [path]
-  (let [clip (AudioSystem/getClip)
-        stream (AudioSystem/getAudioInputStream (java.io.File. path))]
-    (.open clip stream)
-    clip))
+(def sample-rate 44100.0)
+(def bpm 120)
+(def frames-per-8th (int (/ (* sample-rate 60) bpm 2)))
 
-(defn play [^Clip clip]
-  (.setFramePosition clip 0)
-  (.start clip))
+(def output-format (AudioFormat. sample-rate 16 2 true false))
+
+(defn load-sample [path]
+  (let [stream (AudioSystem/getAudioInputStream (java.io.File. path))
+        src-format (.getFormat stream)
+        target-format (AudioFormat. AudioFormat$Encoding/PCM_SIGNED
+                                    (.getSampleRate src-format)
+                                    16
+                                    (.getChannels src-format)
+                                    (* 2 (.getChannels src-format))
+                                    (.getSampleRate src-format)
+                                    false)
+        converted (AudioSystem/getAudioInputStream target-format stream)
+        bytes (byte-array (.available converted))]
+    (.read converted bytes)
+    (.close converted)
+    {:bytes bytes :channels (.getChannels src-format)}))
+
+(defn sample->stereo-ints [{:keys [bytes channels]}]
+  (let [n-samples (/ (alength bytes) 2)
+        n-frames (/ n-samples channels)
+        out (int-array (* 2 n-frames))]
+    (dotimes [i n-frames]
+      (let [left (+ (bit-and (aget bytes (* i channels 2)) 0xFF)
+                    (bit-shift-left (aget bytes (+ (* i channels 2) 1)) 8))]
+        (aset out (* 2 i) left)
+        (aset out (+ (* 2 i) 1) (if (= channels 2)
+                                   (+ (bit-and (aget bytes (+ (* i channels 2) 2)) 0xFF)
+                                      (bit-shift-left (aget bytes (+ (* i channels 2) 3)) 8))
+                                   left))))
+    out))
+
+(defn ints->bytes [^ints arr]
+  (let [out (byte-array (* 2 (alength arr)))]
+    (dotimes [i (alength arr)]
+      (let [v (aget arr i)]
+        (aset out (* 2 i) (unchecked-byte (bit-and v 0xFF)))
+        (aset out (+ (* 2 i) 1) (unchecked-byte (bit-shift-right v 8)))))
+    out))
+
+(defn mix-sample [^ints buffer ^ints sample]
+  (let [len (min (alength sample) (alength buffer))]
+    (dotimes [i len]
+      (let [mixed (+ (aget buffer i) (aget sample i))]
+        (aset buffer i (int (max -32768 (min 32767 mixed))))))))
 
 (defn -main []
-  (let [kick (load-clip "samples/BD Kick 006 HC.wav")
-        snare (load-clip "samples/SN Sd 4Bit Vinyl St GB.wav")
-        hh (load-clip "samples/HH 60S Stomp2 GB.wav")
-        interval 400]
+  (let [info (DataLine$Info. SourceDataLine output-format)
+        line (AudioSystem/getLine info)
+        kick (sample->stereo-ints (load-sample "samples/BD Kick 006 HC.wav"))
+        snare (sample->stereo-ints (load-sample "samples/SN Sd 4Bit Vinyl St GB.wav"))
+        hh (sample->stereo-ints (load-sample "samples/HH 60S Stomp2 GB.wav"))
+        samples-per-8th (* frames-per-8th 2)]
+    (.open ^SourceDataLine line output-format)
+    (.start ^SourceDataLine line)
     (println "Playing 4/4 (Ctrl+C to stop)")
     (loop [beat 0]
-      (play hh)
-      (case (mod beat 4)
-        0 (play kick)
-        2 (play snare)
-        nil)
-      (Thread/sleep interval)
+      (let [buffer (int-array samples-per-8th)]
+        (mix-sample buffer hh)
+        (case (mod beat 4)
+          0 (mix-sample buffer kick)
+          2 (mix-sample buffer snare)
+          nil)
+        (let [out (ints->bytes buffer)]
+          (.write ^SourceDataLine line out 0 (alength out))))
       (recur (inc beat)))))
